4a54dd1a9f6de4073ccc0e010879c15f4b24991d
Vu Tran <baysao@gmail.com>
2021-12-10 08:33:12 +0700

    Split prometheus seperate for gw & node

---
+++ config.lua
@@ -0,0 +1,32 @@
+local _config = {
+    server = {
+        nginx = {
+            port = "80",
+            port_ssl = "443",
+            server_name = "massbitroute"
+        }
+    },
+    templates = {},
+    apps = {},
+    supervisor = [[
+
+[program:stat_prometheus_gw]
+command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_gw _SITE_ROOT_ v1
+autorestart=true
+redirect_stderr=true
+stdout_logfile=_SITE_ROOT_/logs/portal_prometheus_gw.log
+
+[program:stat_prometheus_node]
+command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_node _SITE_ROOT_ v1
+autorestart=true
+redirect_stderr=true
+stdout_logfile=_SITE_ROOT_/logs/portal_prometheus_node.log
+
+[program:stat_grafana]
+command=/bin/bash _SITE_ROOT_/scripts/run loop _service_grafana _SITE_ROOT_ v1
+autorestart=true
+redirect_stderr=true
+stdout_logfile=_SITE_ROOT_/logs/portal_grafana.log
+    ]]
+}
+return _config
