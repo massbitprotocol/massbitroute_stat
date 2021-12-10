6ce9707ca940d53cbde0ffcc180eb3c2b65d7e61
Vu Tran <baysao@gmail.com>
2021-12-10 11:06:26 +0700

    Add monitor, reload when git change

--- config.lua
+++ config.lua
@@ -14,19 +14,19 @@ local _config = {
 command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_gw _SITE_ROOT_ v1
 autorestart=true
 redirect_stderr=true
-stdout_logfile=_SITE_ROOT_/logs/portal_prometheus_gw.log
+stdout_logfile=_SITE_ROOT_/logs/stat_prometheus_gw.log
 
 [program:stat_prometheus_node]
 command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_node _SITE_ROOT_ v1
 autorestart=true
 redirect_stderr=true
-stdout_logfile=_SITE_ROOT_/logs/portal_prometheus_node.log
+stdout_logfile=_SITE_ROOT_/logs/stat_prometheus_node.log
 
 [program:stat_grafana]
 command=/bin/bash _SITE_ROOT_/scripts/run loop _service_grafana _SITE_ROOT_ v1
 autorestart=true
 redirect_stderr=true
-stdout_logfile=_SITE_ROOT_/logs/portal_grafana.log
+stdout_logfile=_SITE_ROOT_/logs/stat_grafana.log
     ]]
 }
 return _config
