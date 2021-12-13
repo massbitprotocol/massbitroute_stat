78903f5021c72d9f00399d190869ce9e2e8fca4f
Vu Tran <baysao@gmail.com>
2021-12-13 11:58:14 +0700

    Seperate two prometheus Node and GW in instance & port -> stat.mbr.massbitroute.com

--- config.lua
+++ config.lua
@@ -10,12 +10,14 @@ local _config = {
     apps = {},
     supervisor = [[
 
+; Prometheus for Community Gateway
 [program:stat_prometheus_gw]
 command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_gw _SITE_ROOT_ v1
 autorestart=true
 redirect_stderr=true
 stdout_logfile=_SITE_ROOT_/logs/stat_prometheus_gw.log
 
+; Prometheus for Community Node
 [program:stat_prometheus_node]
 command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_node _SITE_ROOT_ v1
 autorestart=true
