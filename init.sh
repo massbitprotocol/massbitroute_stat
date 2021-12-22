24f78a55ca3c563f84af1448ee0384d809cff232
Vu Tran <baysao@gmail.com>
2021-12-23 04:56:03 +0700

    Improve scripts/run for automation update conf

--- init.sh
+++ init.sh
@@ -3,7 +3,11 @@ apt update
 apt install -y python-is-python2 supervisor
 mkdir -p /massbit/massbitroute/app/
 git clone http://mbr_gateway:6a796299bb72357770735a79019612af228586e7@git.massbitroute.com/massbitroute/gbc.git /massbit/massbitroute/app/gbc
+
 ln -sf /massbit/massbitroute/app/gbc/bin/openresty /usr/local/
+
+git clone http://mbr_gateway:6a796299bb72357770735a79019612af228586e7@git.massbitroute.com/massbitroute/ssl.git /etc/letsencrypt
+
 git clone http://mbr_gateway:6a796299bb72357770735a79019612af228586e7@git.massbitroute.com/massbitroute/asdf.git /massbit/massbitroute/app/gbc/bin/.asdf
 cd $(dirname $(realpath $0))
 ln -sf /massbit/massbitroute/app/gbc
