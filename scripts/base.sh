b312a42413bc7d1c7f25f829735c03d46e20ef3d
Vu Tran <baysao@gmail.com>
2022-03-22 11:11:25 +0700

    seperate functions util in base.sh

---
+++ scripts/base.sh
@@ -0,0 +1,8 @@
+_git_clone() {
+	_url=$1
+	_dir=$2
+	if [ -d "$_dir" ]; then rm -rf $_dir; fi
+	mkdir -p $_dir
+	git clone $_url $_dir
+	# git -C $_dir remote set-url origin $_url
+}
--- scripts/install_private.sh
+++ scripts/install_private.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 SITE_ROOT=$(realpath $(dirname $(realpath $0))/..)
 source $SITE_ROOT/.env.$(cat $SITE_ROOT/var/ENV)
-_git_clone() {
-	_url=$1
-	_dir=$2
-	if [ -d "$_dir" ]; then rm -rf $_dir; fi
-	mkdir -p $_dir
-	git clone $_url $_dir
-	git -C $_dir remote set-url origin $_url
-}
-
+source $SITE_ROOT/scripts/base.sh
 _git_clone http://${GIT_READ}@git.massbitroute.com/massbitroute/${ssl}.git /etc/letsencrypt
