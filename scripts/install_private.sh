41fae34b3c1a1a102817d3e303bf45fc34e3c69d
Vu Tran <baysao@gmail.com>
2022-03-22 11:20:05 +0700

    fix ssl repo name

--- scripts/install_private.sh
+++ scripts/install_private.sh
@@ -3,4 +3,4 @@ SITE_ROOT=$(realpath $(dirname $(realpath $0))/..)
 env=$(cat $SITE_ROOT/vars/ENV)
 source $SITE_ROOT/.env.$env
 source $SITE_ROOT/scripts/base.sh
-_git_clone http://${GIT_READ}@git.${DOMAIN}/massbitroute/${ssl}.git /etc/letsencrypt
+_git_clone http://${GIT_READ}@git.${DOMAIN}/massbitroute/ssl.git /etc/letsencrypt
