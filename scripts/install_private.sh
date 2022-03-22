53228c42e1f858f60706e94ef167a749c9eeddf3
Vu Tran <baysao@gmail.com>
2022-03-22 11:07:15 +0700

    add .env.dev

--- scripts/install_private.sh
+++ scripts/install_private.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 SITE_ROOT=$(realpath $(dirname $(realpath $0))/..)
-auth=massbit:41d919e74993945a97972d147c4d01847e8bc1b6
-service=ssl
-git clone http://${auth}@git.massbitroute.com/massbitroute/${service}.git /etc/letsencrypt
-git -C /etc/letsencrypt remote set-url origin http://$user:$pass@git.massbitroute.com/massbitroute/${service}.git
+source $SITE_ROOT/.env.$(cat $SITE_ROOT/var/ENV)
+_git_clone() {
+	_url=$1
+	_dir=$2
+	if [ -d "$_dir" ]; then rm -rf $_dir; fi
+	mkdir -p $_dir
+	git clone $_url $_dir
+	git -C $_dir remote set-url origin $_url
+}
+
+_git_clone http://${GIT_READ}@git.massbitroute.com/massbitroute/${ssl}.git /etc/letsencrypt
