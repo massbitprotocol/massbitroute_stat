c4e1ba3840f36dceed373827d603ed938b86cd11
Vu Tran <baysao@gmail.com>
2022-05-06 11:41:16 +0700

    Update supervisor conf

--- config.lua
+++ config.lua
@@ -32,7 +32,25 @@ killasgroup=true
 stopsignal=INT
 stdout_logfile=_SITE_ROOT_/logs/stat_prometheus_gw.log
 
+[program:stat_prometheus_gw_v1]
+command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_gw_v1 _SITE_ROOT_ v1
+autorestart=true
+redirect_stderr=true
+stopasgroup=true
+killasgroup=true
+stopsignal=INT
+stdout_logfile=_SITE_ROOT_/logs/stat_prometheus_gw_v1.log
+
 ; Prometheus for Community Node
+[program:stat_prometheus_node_v1]
+command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_node_v1 _SITE_ROOT_ v1
+autorestart=true
+redirect_stderr=true
+stopasgroup=true
+killasgroup=true
+stopsignal=INT
+stdout_logfile=_SITE_ROOT_/logs/stat_prometheus_node_v1.log
+
 [program:stat_prometheus_node]
 command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_node _SITE_ROOT_ v1
 autorestart=true
