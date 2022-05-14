b670f5a70ec963e318d764a617ed07f870aaaad3
Vu Tran <baysao@gmail.com>
2022-05-14 11:40:59 +0700

    Update service config & location conf
    * Remove prometheus old version service
    * Remove location prometheus old version & grafana

--- config.lua
+++ config.lua
@@ -20,18 +20,17 @@ stopsignal=INT
 stdout_logfile=_SITE_ROOT_/../mkagent/logs/monitor_client.log
     ]]
     },
-    supervisor = [[
-
-; Prometheus for Community Gateway
-[program:stat_prometheus_gw]
-command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_gw _SITE_ROOT_ v1
+    supervisor_tmp = [[
+[program:stat_grafana]
+command=/bin/bash _SITE_ROOT_/scripts/run loop _service_grafana _SITE_ROOT_ v1
 autorestart=true
 redirect_stderr=true
 stopasgroup=true
 killasgroup=true
 stopsignal=INT
-stdout_logfile=_SITE_ROOT_/logs/stat_prometheus_gw.log
-
+stdout_logfile=_SITE_ROOT_/logs/stat_grafana.log
+]],
+    supervisor = [[
 [program:stat_prometheus_gw_v1]
 command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_gw_v1 _SITE_ROOT_ v1
 autorestart=true
@@ -50,24 +49,6 @@ stopasgroup=true
 killasgroup=true
 stopsignal=INT
 stdout_logfile=_SITE_ROOT_/logs/stat_prometheus_node_v1.log
-
-[program:stat_prometheus_node]
-command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus_node _SITE_ROOT_ v1
-autorestart=true
-redirect_stderr=true
-stopasgroup=true
-killasgroup=true
-stopsignal=INT
-stdout_logfile=_SITE_ROOT_/logs/stat_prometheus_node.log
-
-[program:stat_grafana]
-command=/bin/bash _SITE_ROOT_/scripts/run loop _service_grafana _SITE_ROOT_ v1
-autorestart=true
-redirect_stderr=true
-stopasgroup=true
-killasgroup=true
-stopsignal=INT
-stdout_logfile=_SITE_ROOT_/logs/stat_grafana.log
     ]]
 }
 return _config
